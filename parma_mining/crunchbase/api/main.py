c03efdc786facc84eedc33878fe1f3f9ef217c40
Ege Kocabas <48245934+egekocabas@users.noreply.github.com>
2024-01-10 01:38:01 +0100

    fix: Setup logging (#6)

--- parma_mining/crunchbase/api/main.py
+++ parma_mining/crunchbase/api/main.py
@@ -1,6 +1,20 @@
 """Main entrypoint for the API routes in of parma-analytics."""
+import logging
+import os
 
 from fastapi import FastAPI, status
+
+env = os.getenv("env", "local")
+
+if env == "prod":
+    logging.basicConfig(level=logging.INFO)
+elif env in ["staging", "local"]:
+    logging.basicConfig(level=logging.DEBUG)
+else:
+    logging.warning(f"Unknown environment '{env}'. Defaulting to INFO level.")
+    logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
@@ -8,4 +22,5 @@ app = FastAPI()
 @app.get("/", status_code=status.HTTP_200_OK)
 def root():
     """Root endpoint for the API."""
+    logger.debug("Root endpoint called")
     return {"welcome": "at parma-mining-crunchbase"}
